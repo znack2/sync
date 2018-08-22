21a83c045c015674d1f9deb5cbc84d54fe3fede2
Antony Minkowski <vertex-3d@mail.ru>
2018-08-22 09:56:48 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,10 @@ Route::group([
     // 'middleware' => 'checkToken'
 ], function () {
 
-	Route::get('/test', function () {
-		echo "test ok";						
-    });
-
 	Route::post('/syncEngine', 						
     	['uses' => 'SyncController@syncEngine', 							
     	 'as' 	=> 'sync.syncEngine'
 	]);
-
-	// Route::post('/create-channel', 						
- //    	['uses' => 'SyncController@createChannel', 							
- //    	 'as' 	=> 'sync.createChannel'
-	// ]);
-
-	// Route::delete('/v1/syncEngine/delete-channel', 
-	// 	['uses' => 'SyncController@deleteChannel',
-	// 	 'as' 	=> 'sync.deleteChannel'
-	// ]);
-
-	// Route::get('/accounts', 						
- //    	['uses' => 'SyncController@accounts', 							
- //    	 'as' 	=> 'sync.accounts'
-	// ]);
 });
 
 
--- src/Controllers/SyncController.php
+++ src/Controllers/SyncController.php
@@ -10,18 +10,16 @@ use Usedesk\SyncIntegration\Services\SyncEngineEmail;
 
 
 use App\Jobs\Client\{FindOrCreateClient};
-use App\Jobs\Ticket\{FindOrCreateTicket,CheckDouble};
+use App\Jobs\Ticket\{FindOrCreateTicket};//CheckDouble
 use App\Jobs\Comment\{AddComment};
 
 class SyncController
 {
-    private $addr;
     private $service;
 
     public function __construct()
     {
         $this->service = new SyncEngineService();
-        $this->addr = $this->service->getAddr();
     }
 
 	 /**
@@ -40,50 +38,86 @@ class SyncController
                 continue;
             }
 
-            $this->createTicketFromSync($item['attributes']);
+            $data = prepare($item['attributes']);
+
+            $data['date'] = $this->setDate($item['attributes']['date']);
+
+            $files = $this->setFiles($item['attributes']['files']);
+
+            $channel = $this->setChannel($item['attributes']['to'][0]['email']);
+
+            $owner = [ 
+                'type' => 'App\Models\Client\Client', 
+                'id'   => $this->setClient($item['attributes']['from'],$channel->company_id);
+            ];
+
+            $this->setTicket($company_id,$thread_id,$channel_id,$item['attributes'],$owner);
+
+            $this->sendComment($company_id,$ticket_id,$owner,$from,$data,$files);
         }
 
         return 'ok';
     }
 
-    /**
-     * @param $attributes
-     * @return bool
-     */
-    public function createTicketFromSync($attributes) 
+
+    // if (dispatch_now(new CheckDouble($channel->company_id, $client_id, $data['subject'], $data['date']))) {
+    //     return false;
+    // }
+
+    private function prepare(array $attributes)
     {
-            $data = [];
-            $need = ['body', 'from', 'account_id', 'thread_id', 'snippet', 'id', 'subject'];
+        $data = [];
 
-            foreach ($need as $key) {
-                $row = $attributes[$key] ?? null;
+        $need = ['body', 'from', 'account_id', 'thread_id', 'snippet', 'id', 'subject'];
 
-                if ($row === null) {
-                    continue;
-                }
+        foreach ($need as $key) {
+            $row = $attributes[$key] ?? null;
 
-                if (is_array($key) or is_object($key)) {
-                    $data[$key] = json_encode($row);
-                    continue;
-                }
-
-                $data[$key] = $row;
+            if ($row === null) {
+                continue;
             }
 
-            if (!empty($attributes['date'])) {
-                $data['date'] = date("Y-m-d H:i:s", $attributes['date']['$date']);
+            if (is_array($key) or is_object($key)) {
+                $data[$key] = json_encode($row);
+                continue;
+            }
+
+            $data[$key] = $row;
+        }
+
+        return $data;
+    }
+
+    private function setDate(array $date)
+    {
+        if (!empty($date)) {
+                $data = date("Y-m-d H:i:s", $date['$date']);
             } else {
-                $data['date'] = date("Y-m-d H:i:s");
+                $data = date("Y-m-d H:i:s");
             }
+        return $data;
+    }
 
-            $files = [];
+    private function setChannel(string $email)
+    {
+        $channel = DB::table('email_channels')->where('incoming_email', $email)->first();
 
-            if (isset($attributes['files'])) {
-                foreach ($attributes['files'] as $file) {
+        return $channel;
+    }
+
+    private function setFiles(array $files): array
+    {
+        $result = [];
+
+            if (isset($files)) {
+                foreach ($files as $file) {
                     if (isset($file['id'])) {
                         $file_id = $file['id'];
+
+                        $url = env('SYC_ENGINE_HOST', 'localhost') . ':5555';
+
                         $file_data = [
-                            'url' => 'http://' . ($data['account_id']) . '@' . $this->addr .'/files/' . $file_id . '/download',
+                            'url' => 'http://' . ($data['account_id']) . '@' . $url .'/files/' . $file_id . '/download',
                             'filename' => $file['filename'],
                         ];
 
@@ -91,99 +125,83 @@ class SyncController
                             $file_data['content_id'] = $file['content_id'];
                         }
 
-                        $files[] = $file_data;
+                        $result[] = $file_data;
                     }
                 }
             }
-            $channel = $this->getChannel($attributes['to'][0]['email']);
-
-            $name = $attributes['from'][0]['name'] ?? '';
-            $name = explode(' ', $name);
-
-            $params = [
-	            'email' => $attributes['from'][0]['email'],
-	            'client' => ['company_id' => $channel->company_id],
-	            'first_name' => !empty($name[0]) ? $name[0] : $attributes['from'][0]['email'],
-	            'last_name' => $name[1] ?? '',
-	        ];
-
-            $client_id = dispatch_now(new FindOrCreateClient(
-                $channel->company_id,
-                'email',
-                $params
-            ));
-
-            if (dispatch_now(new CheckDouble($channel->company_id, $client_id, $data['subject'], $data['date']))) {
-                return false;
-            }
-
-            $owner = [ //owner
-	            'type' => 'App\Models\Client\Client', //owner
-	            'id' => $client_id,
-	        ];
-
-            if (!$ticket_id = $this->findTicketById($attributes['body'], $attributes['thread_id'])) {
-                $ticket = dispatch_now(new FindOrCreateTicket(
-                    $channel->company_id,
-                    $owner,
-                    $channel->id,
-                    'email', //channel type
-                    $attributes['thread_id'],
-                    $attributes['subject'],
-                    $data['date'] ?? date("Y-m-d H:i:s")
-                ));
-
-                $ticket_id = $ticket->id;
-            }
-
-            $requestData = [ 
-	            'user_type' => 'client',
-	            'message_type' => 'public',
-	            'message' => $attributes['body'],
-	            'subject' => $attributes['subject'],
-	            'is_html' => true,
-	            'has_file' => !empty($files),
-	            'all_data' => $data,
-
-	            'channel_type' => 'email',
-	            'channel_id' => $channel->id,
-	            'from_client' => [
-	                $client_id => $attributes['from'][0]['email']
-	            ]
-	        ];
-
-            dispatch(new \App\Jobs\Comment\AddComment(
-                $channel->company_id,
-                $ticket_id,
-                $user_id = 0,
-                $owner,
-   				$requestData,
-                $files
-            ));
-        }
+        return $result;
     }
 
-    protected function getChannel($email)
-	{
-        $channel = DB::table('email_channels')->where('incoming_email', $email)->first();
-
-        if (!$channel) {
-            throw new \Exception('Channel not found - ' . $email);
-        }
-
-        return $channel;
-    }
-
-    protected function findTicketById($message, $thread_id)
+    private function setClient(array $from,int $company_id): int
     {
-        preg_match('/\<span ticket_id="([0-9]+)">/i', $message, $matches);
+        $name = $from[0]['name'] ?? '';
+        $name = explode(' ', $name);
 
+        $params = [
+            'email'      => $from[0]['email'],
+            'client'     => ['company_id' => $company_id],
+            'first_name' => !empty($name[0]) ? $name[0] : $from[0]['email'],
+            'last_name'  => $name[1] ?? '',
+        ];
+
+        $client_id = dispatch_now(new FindOrCreateClient(
+            $company_id,
+            'email',
+            $params
+        ));
+
+        return $client_id;
+    }
+
+    private function sendComment(int $company_id, int $ticket_id, array $owner,array $from,array $data,array $files = []): int
+    {
+        $requestData = [ 
+            // 'user_type'    => 'client',
+            'message_type' => 'public',
+            'message' => $attributes['body'],
+            'subject' => $attributes['subject'],
+            'is_html' => true,
+            'has_file' => !empty($files),
+            'all_data' => $data,
+            'channel'=>[
+                'channel_type' => 'email',
+                'channel_id' => $channel->id,
+            ],
+            'from_client' => [
+                $client_id => $from[0]['email']
+            ]
+        ];
+
+        dispatch(new AddComment(
+            $company_id,
+            $ticket_id,
+            $user_id = 0,
+            $owner,
+            $requestData,
+            $files
+        ));
+    }
+
+
+    private function setTicket(int $company_id, int $thread_id, int $channel_id, array $attributes,array $owner): int
+    {
+        preg_match('/\<span ticket_id="([0-9]+)">/i', $attributes['body'], $matches);
+
+        //TODO: remove repository
         if ($matches) {
-            DB::table('tickets')->where('id', $matches[1])->update(['thread_id' => $thread_id]);
+            DB::table('tickets')->where('id', $matches[1])->update(['thread_id' => $attributes['thread_id']]);
             return $matches[1];
         }
 
-        return null;
+        $ticket = dispatch_now(new FindOrCreateTicket(
+            $company_id,
+            $owner,
+            $channel_id,
+            $channel_type = 'email',
+            $attributes['thread_id'],
+            $attributes['subject'],
+            $data['date']
+        ));
     }
 
 }
